c6f764b3b23e88e0ad54d87fdd0ec04b344e371a
Rizal <rizalsofiana1976@gmail.com>
2025-07-12 19:36:37 +0700

    update SchoolCashflowDAO

--- src/model/DAO/SchoolCashflowDAO.java
+++ src/model/DAO/SchoolCashflowDAO.java
@@ -5,8 +5,6 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SchoolCashflowDAO {
     private Connection conn;
