3abc4b57ddd28feed738d79dbe196e29293285bb
Rizal <rizalsofiana1976@gmail.com>
2025-07-13 14:17:53 +0700

    membuat InvoiceController

---
+++ src/controller/InvoiceController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import config.DatabaseConnection;
+import java.sql.Connection;
+import java.time.LocalDate;
+import model.Invoice;
+import model.DAO.InvoiceDAO;
+
+public class InvoiceController {
+    private InvoiceDAO invoiceDAO;
+
+    public InvoiceController() {
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            invoiceDAO = new InvoiceDAO(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean createInvoice(int student_id, float jumlah) {
+        LocalDate nextMonth = LocalDate.now().plusMonths(1);
+        LocalDate tanggalJatuhTempo = nextMonth.withDayOfMonth(nextMonth.lengthOfMonth());
+
+        Invoice invoice = new Invoice();
+        invoice.setStudentId(student_id);
+        invoice.setJumlah(jumlah);
+        invoice.setTanggalJatuhTempo(tanggalJatuhTempo);
+        invoice.setStatus("belum_lunas");
+    }
+}
