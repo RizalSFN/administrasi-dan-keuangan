4d18193627425b72d7ed3b29e042822cd9923dc9
Rizal <rizalsofiana1976@gmail.com>
2025-07-13 15:07:50 +0700

    update SchoolIncomeController

--- src/controller/SchoolInconeController.java
+++ src/controller/SchoolInconeController.java
@@ -4,6 +4,7 @@ import model.SchoolIncome;
 import model.DAO.SchoolIncomeDAO;
 import config.DatabaseConnection;
 import java.sql.Connection;
+import java.sql.Date;
 import java.time.LocalDate;
 
 public class SchoolInconeController {
@@ -18,17 +19,28 @@ public class SchoolInconeController {
         }
     }
 
-    public boolean createSchoolIncome(int sourceId, float jumlah, String buktiTransaksi, String keterangan, int created_by) {
+    public boolean createSchoolIncome(int sourceId, float jumlah, String buktiTransaksi, String keterangan,
+            int created_by) {
         SchoolIncome schoolIncome = new SchoolIncome();
         schoolIncome.setSourceId(sourceId);
         schoolIncome.setJumlah(jumlah);
         schoolIncome.setTanggalPemasukan(LocalDate.now());
-        schoolIncome.setBuktiTransaksi(buktiTransaksi); 
+        schoolIncome.setBuktiTransaksi(buktiTransaksi);
         schoolIncome.setKeterangan(keterangan);
         schoolIncome.setCreatedBy(created_by);
 
         return schoolIncomeDAO.insertNewSchoolIncome(schoolIncome);
     }
 
-    public 
+    public boolean updateSchoolIncome(SchoolIncome schoolIncome) {
+        return schoolIncomeDAO.updateSchoolIncome(schoolIncome);
+    }
+
+    public SchoolIncome getSchoolIncomeBySourceId(int sourceId) {
+        return schoolIncomeDAO.findBySourceId(sourceId);
+    }
+
+    public SchoolIncome getSchoolIncomeByTanggalPemasukan(Date tanggalPemasukan) {
+        return schoolIncomeDAO.findByTanggalPemasukan(tanggalPemasukan);
+    }
 }
