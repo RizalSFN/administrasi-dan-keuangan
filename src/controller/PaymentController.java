a4266f10f88888cb67e1694ad1177c5641e10518
Rizal <rizalsofiana1976@gmail.com>
2025-07-20 20:26:24 +0700

    update payment controller

--- src/controller/PaymentController.java
+++ src/controller/PaymentController.java
@@ -12,9 +12,11 @@ import java.sql.Connection;
 import java.time.LocalDate;
 
 public class PaymentController {
+
     private PaymentDAO paymentDAO;
     private InvoiceDAO invoiceDAO;
     private TransactionHistoryDAO transactionHistoryDAO;
+    private InvoiceController invoiceController;
 
     public PaymentController() {
         try {
@@ -22,58 +24,17 @@ public class PaymentController {
             paymentDAO = new PaymentDAO(conn);
             invoiceDAO = new InvoiceDAO(conn);
             transactionHistoryDAO = new TransactionHistoryDAO(conn);
+            invoiceController = new InvoiceController();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public boolean cashPayment(int invoice_id, float jummlah_bayar) {
-        Payment payment = new Payment();
-        payment.setInvoiceId(invoice_id);
-        payment.setBuktiPembayaran(null);
-        payment.setTanggalBayar(LocalDate.now());
-        payment.setJumlahBayar(jummlah_bayar);
-        payment.setJenisPembayaran("tunai");
-        payment.setStatusVerifikasi("menunggu");
-
+    public boolean createPayment(Payment payment) {
         int paymentId = paymentDAO.insertNewPayment(payment);
-
         if (paymentId > 0) {
-            int studentId = invoiceDAO.findById(invoice_id);
-
-            TransactionHistory transactionHistory = new TransactionHistory();
-            transactionHistory.setStudentId(studentId);
-            transactionHistory.setPaymentId(paymentId);
-            transactionHistory.setKeterangan("Pembayaran tunai SPP");
-
-            return transactionHistoryDAO.insertNewTransactionHistory(transactionHistory);
+            return invoiceController.updateInvoice(payment.getInvoiceId(), "lunas");
         }
-
-        return false;
-    }
-
-    public boolean cashlessPayment(int invoice_id, float jumlah_bayar, String bukti_pembayaran) {
-        Payment payment = new Payment();
-        payment.setInvoiceId(invoice_id);
-        payment.setBuktiPembayaran(bukti_pembayaran);
-        payment.setTanggalBayar(LocalDate.now());
-        payment.setJumlahBayar(jumlah_bayar);
-        payment.setJenisPembayaran("non tunai");
-        payment.setStatusVerifikasi("menunggu");
-
-        int paymentId = paymentDAO.insertNewPayment(payment);
-
-        if (paymentId > 0) {
-            int studentId = invoiceDAO.findById(invoice_id);
-
-            TransactionHistory transactionHistory = new TransactionHistory();
-            transactionHistory.setStudentId(studentId);
-            transactionHistory.setPaymentId(paymentId);
-            transactionHistory.setKeterangan("Pembayaran non tunai SPP");
-
-            return transactionHistoryDAO.insertNewTransactionHistory(transactionHistory);
-        }
-
         return false;
     }
 
